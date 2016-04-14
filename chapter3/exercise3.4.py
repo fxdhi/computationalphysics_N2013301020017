5a336c25f2ec788e142a9449e89ff55a4d55b102
fxdhi <1813476287@qq..com>
2016-04-14 17:09:39 +0800

    1

--- chapter3/exercise3.4.py
+++ chapter3/exercise3.4.py
@@ -30,7 +30,10 @@ class harmonic():
             self.v.append(self.v[i - 1]-k*self.x[i-1]**self.alpha*self.dt)
             self.x.append(self.x[i - 1]+self.v[i]*self.dt)
             self.t.append(self.t[i - 1] + self.dt)
+        print self.x
         return 0
+        
+       
     def plot2D(self,color):
         pl.plot(self.t, self.x,color,label="$v0=%dm/s$,$\\alpha=%d$"%(self.v0,self.alpha))
         pl.title("motion state")
@@ -41,21 +44,21 @@ class harmonic():
 a = harmonic(1, 1, 10,0.1,20)
 a.calculate()
 a.plot2D("red")
-b = harmonic(1, 1, 20,0.01,20)
+b = harmonic(1, 1, 20,0.1,20)
 b.calculate()
 b.plot2D("green")
-c = harmonic(1, 1, 30,0.01,20)
+c = harmonic(1, 1, 30,0.1,20)
 c.calculate()
 c.plot2D("cyan")
 pl.legend()
 pl.show()
-A = harmonic(3, 2, 10,0.01,10)
+A = harmonic(3, 2, 10,0.1,10)
 A.calculate()
 A.plot2D("red")
-B = harmonic(3, 2, 20,0.01,10)
+B = harmonic(3, 2, 20,0.1,10)
 B.calculate()
 B.plot2D("blue")
-C = harmonic(3, 2, 0,0.01,10)
+C = harmonic(3, 2, 0,0.1,10)
 C.calculate()
 C.plot2D("purple")
 pl.legend()
